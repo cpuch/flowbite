48b7ea57051f8b3a299336035bb0da8d44e0ceb0
Cedric Puchalver <cedric@season-of-mist.com>
2025-02-23 17:03:52 +0100

    Component Alert

--- src/View/Components/Alert.php
+++ src/View/Components/Alert.php
@@ -8,12 +8,24 @@ use Illuminate\View\Component;
 
 class Alert extends Component
 {
+    public $id;
+
+    public $type;
+
+    public $icon;
+
+    public $border;
+
     /**
      * Create a new component instance.
      */
-    public function __construct(
-        public string $type = 'info',
-    ) {}
+    public function __construct(string $id = '', string $type = 'info', bool $icon = false, bool $border = false)
+    {
+        $this->id = $id;
+        $this->type = $type;
+        $this->icon = $icon;
+        $this->border = $border;
+    }
 
     /**
      * Get the view / contents that represent the component.
